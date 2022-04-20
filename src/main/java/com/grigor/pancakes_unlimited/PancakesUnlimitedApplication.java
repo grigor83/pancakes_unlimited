e68593c738cc97db5ec766457bd283f728a3c51f
grigor83 <igorgrubisa@hotmail.com>
2022-04-20 16:09:47 +0200

    create .git

---
+++ src/main/java/com/grigor/pancakes_unlimited/PancakesUnlimitedApplication.java
@@ -0,0 +1,13 @@
+package com.grigor.pancakes_unlimited;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PancakesUnlimitedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PancakesUnlimitedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grigor/pancakes_unlimited/PancakesUnlimitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grigor.pancakes_unlimited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PancakesUnlimitedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
